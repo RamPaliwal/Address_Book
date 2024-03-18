a2b07834c1678aee6669035a8d4b13ca89bdc800
RamPaliwal <rampaliwal2002@gmail.com>
2024-03-18 17:09:42 +0530

    Added UC 2 to add new contacts in Address Book

--- Address_Book.java
+++ Address_Book.java
@@ -1,15 +1,15 @@
 import java.util.*;
 class Contact {
-     String firstName;
-     String lastName;
-     String address;
-     String city;
-     String state;
-     String zip;
-     String phoneNumber;
-     String email;
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phoneNumber;
+    String email;
 
-     Contact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+    Contact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -23,30 +23,53 @@ class Contact {
     public String toString() {
         return
                 "firstName= " + firstName + "\n" +
-                "lastName= " + lastName + "\n" +
-                "address= " + address + "\n" +
-                "city= " + city + "\n" +
-                "state= " + state + "\n" +
-                "zip= " + zip + "\n" +
-                "phoneNumber= " + phoneNumber + "\n" +
-                "email= " + email ;
+                        "lastName= " + lastName + "\n" +
+                        "address= " + address + "\n" +
+                        "city= " + city + "\n" +
+                        "state= " + state + "\n" +
+                        "zip= " + zip + "\n" +
+                        "phoneNumber= " + phoneNumber + "\n" +
+                        "email= " + email ;
     }
 
 }
 
 public class AddressBookSystem {
-
-
+    private static ArrayList<Contact> contacts = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program....");
-        System.out.println("Contact created...");
-        Contact c = new Contact("Nisha","Mali", "a/p Gadmudshingi", "Kolhapur",
-                "Maharashtra", "416119","9808080867", "nishamali@gmail.com");
+        System.out.println("Welcome to Address Book Program.... \n");
 
-        System.out.println("Contacts: \n"+ c);
-
+        AddressBookSystem abs = new AddressBookSystem();
+        abs.addContact();
+        System.out.println("Contacts:");
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
     }
 
+    void addContact() {
+        System.out.print("Enter First Name: ");
+        String firstName = sc.nextLine();
+        System.out.print("Enter Last Name: ");
+        String lastName = sc.nextLine();
+        System.out.print("Enter Address: ");
+        String address = sc.nextLine();
+        System.out.print("Enter City: ");
+        String city = sc.nextLine();
+        System.out.print("Enter State: ");
+        String state = sc.nextLine();
+        System.out.print("Enter Zip: ");
+        String zip = sc.nextLine();
+        System.out.print("Enter Phone Number: ");
+        String phoneNumber = sc.nextLine();
+        System.out.print("Enter Email: ");
+        String email = sc.nextLine();
 
+        Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+        contacts.add(contact);
+        System.out.println();
+        System.out.println("Contact added successfully! \n");
+    }
 }
